e1ea4aa673c652bbf4303c215f3198d3c9a06dc5
Lucho Romero <go.romerolae@gmail.com>
2022-07-14 11:00:33 -0500

    Added several features such as form and navbar sections improved

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,33 @@
-function toggleMenu() {
-	const menu = document.getElementById('main-menu');
-	menu.classList.toggle('hidden');
-}
+const navBar = document.getElementById('navBar');
+const btnMenu = document.getElementById('btnMenu');
+const heroMenu = document.getElementById('heroMenu');
+const sections = document.querySelectorAll('section[id]');
+const form = document.getElementById('form');
+const logo1 = document.getElementById('logo');
+
+/* Header agregar/quitar fondo */
+window.addEventListener('scroll', function () {
+	if (window.scrollY >= 60) {
+		navBar.classList.add('nav-fixed');
+		heroMenu.classList.add('linkwhite');
+		logo1.classList.add('linkwhite');
+		btnMenu.classList.add('linkwhite');
+	} else {
+		navBar.classList.remove('nav-fixed');
+		heroMenu.classList.remove('linkwhite');
+		logo1.classList.remove('linkwhite');
+		btnMenu.classList.remove('linkwhite');
+	}
+});
+
+/* ==========toggle menu ========*/
+
+btnMenu.addEventListener('click', function () {
+	heroMenu.classList.toggle('active');
+});
+
+heroMenu.addEventListener('click', function (e) {
+	if (e.target.matches('.hero__nav-link')) {
+		heroMenu.classList.remove('active');
+	}
+});
